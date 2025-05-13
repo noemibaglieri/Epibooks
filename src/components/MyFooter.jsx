a0c210784d7e978d9149930377a6b6a478d03f42
Noemi Baglieri <noemibaglieri@gmail.com>
2025-05-13 16:28:34 +0200

    footer

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -4,12 +4,38 @@ import { Instagram, Linkedin, TwitterX } from "react-bootstrap-icons";
 const MyFooter = function () {
   return (
     <Container>
-      <Row xs={1} md={2} lg={4}>
+      <Row xs={1} md={2} lg={3} className="text-center">
         <Col>
-          <h5>Follow us</h5>
-          <Linkedin />
-          <Instagram />
-          <TwitterX />
+          <h5 className="footer-title">Resta aggiornato</h5>
+          <ul className="d-flex gap-2 justify-content-center">
+            <li>
+              <Linkedin />
+            </li>
+            <li>
+              <Instagram />
+            </li>
+            <li>
+              <TwitterX />
+            </li>
+          </ul>
+        </Col>
+        <Col className="d-flex flex-column">
+          <h5 className="footer-title">Epibooks</h5>
+          <ul>
+            <li>Via delle Rose, 87</li>
+            <li>04100 - Latina</li>
+            <li>P.IVA: 41351960780</li>
+          </ul>
+        </Col>
+        <Col>
+          <h5 className="footer-title">Legal</h5>
+          <ul>
+            <li>Privacy Policy</li>
+            <li>Shipment & Returns</li>
+            <li>FAQ</li>
+            <li>Work with us</li>
+            <li>Contacts</li>
+          </ul>
         </Col>
       </Row>
     </Container>
